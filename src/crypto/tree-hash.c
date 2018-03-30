ca94f04e3f14facd1bd9d81680a30b5b8c8f825d
Yerko Bits <yerkobits@gmail.com>
2018-03-30 14:52:15 -0300

    Update tree-hash.c

--- src/crypto/tree-hash.c
+++ src/crypto/tree-hash.c
@@ -1,6 +1,6 @@
-// Copyright (c) 2011-2016 The Cryptonote developers
-// Distributed under the MIT/X11 software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Copyright (c) 2012-2017, The CryptoNote developers
+// Copyleft (c) 2016-2018, Prosus Corp RTD
+// Distributed under the MIT/X11 software license.
 
 #include <assert.h>
 #include <stddef.h>
