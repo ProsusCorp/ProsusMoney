431de6b8ddfd046577e490a92f3a96df9e9ef6b4
Yerko Bits <yerkobits@gmail.com>
2018-03-30 14:54:45 -0300

    release v0.5.2

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -1,6 +1,6 @@
 // Copyright (c) 2012-2017, The CryptoNote developers
 // Copyleft (c) 2016-2018, Prosus Corp RTD
-// Distributed under the MIT/X11 software license.
+// Distributed under the MIT/X11 software license
 
 #pragma once
 
