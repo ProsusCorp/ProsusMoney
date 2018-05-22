8d8a5dec5c43315ae15e8115b219431302f84770
Arielony <ariel_saher@hotmail.com>
2018-05-22 15:34:04 -0400

    CORS Headers for HTTP-Server
    Allow all origins to consume it via JS

--- src/HTTP/HttpResponse.cpp
+++ src/HTTP/HttpResponse.cpp
@@ -43,6 +43,7 @@ namespace CryptoNote {
 HttpResponse::HttpResponse() {
   status = STATUS_200;
   headers["Server"] = "CryptoNote-based HTTP server";
+  headers["Access-Control-Allow-Origin"] = "*";
 }
 
 void HttpResponse::setStatus(HTTP_STATUS s) {
