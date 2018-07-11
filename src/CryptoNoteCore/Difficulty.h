dba0b4e6f7c411815367cbca826f3816e4c39ce9
YerkoBits <yerkobits@gmail.com>
2018-07-11 16:38:40 -0400

    Update Difficulty.h

--- src/CryptoNoteCore/Difficulty.h
+++ src/CryptoNoteCore/Difficulty.h
@@ -1,6 +1,6 @@
-// Copyright (c) 2011-2016 The Cryptonote developers
-// Distributed under the MIT/X11 software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Copyright (c) 2012-2017, The CryptoNote developers
+// Copyleft (c) 2016-2018, Prosus Corp RTD
+// Distributed under the MIT/X11 software license
 
 #pragma once
 
@@ -14,4 +14,6 @@ namespace CryptoNote
     typedef std::uint64_t difficulty_type;
 
     bool check_hash(const Crypto::Hash &hash, difficulty_type difficulty);
+// ProsusCorp    
+    difficulty_type next_difficulty(std::vector<std::uint64_t> timestamps, std::vector<difficulty_type> cumulative_difficulties, size_t target_seconds);
 }
