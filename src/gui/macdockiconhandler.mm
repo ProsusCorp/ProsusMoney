08a8d0d9a9bd95828996f572b0d5fad32482612b
YerkoBits <yerkobits@gmail.com>
2018-09-28 14:19:52 -0300

    Update macdockiconhandler.mm

--- src/gui/macdockiconhandler.mm
+++ src/gui/macdockiconhandler.mm
@@ -1,7 +1,6 @@
-// Copyright (c) 2011-2013 The Bitcoin Core developers
-// Copyright (c) 2018 The ParsiCoin developers
-// Distributed under the MIT/X11 software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Copyright (c) 2012-2017, The CryptoNote developers
+// Copyleft (c) 2016-2018, Prosus Corp RTD
+// Distributed under the MIT/X11 software license
 
 #include "macdockiconhandler.h"
 
